976e016bc1140f49bc3cca2e60c8bf3a30a6dfb7
Ivan Pianetti <ipianetti@cent.gov.ar>
2018-06-20 21:42:09 -0300

    Composer json

---
+++ src/QueryBuilder.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ivanpianetti
+ * Date: 19/6/18
+ * Time: 18:58
+ */
+
+namespace QueryBuilder;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class QueryBuilder
+ * @package QueryBuilder
+ * @author  Ivan Pianetti <ivanpianetti23@gmail.com>
+ * @link    https://github.com/bipiane/criteria-builder
+ */
+class QueryBuilder
+{
+}
